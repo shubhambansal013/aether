0aa92dec480ce1dbf13db2c8a87bc34a3bcbc1c8
shubhambansal013 <shubhambansal013@gmail.com>
2025-12-07 00:28:00 +0530

    Send blynk udpates every 60s.

--- pawan.ino
+++ pawan.ino
@@ -30,7 +30,7 @@ const long PING_INTERVAL_MS = 10000;
 
 // --- Sensor Constants ---
 const long SENSOR_BAUD_RATE = 9600;
-const unsigned long BLYNK_SEND_INTERVAL_MS = 1000L;
+const unsigned long BLYNK_SEND_INTERVAL_MS = 60000L;
 const bool USE_MOCK_DATA = false;
 
 // --- Loop delay ---
