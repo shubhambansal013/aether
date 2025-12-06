091869a389a28b57ece0efdf435689f1da722baa
shubhambansal013 <shubhambansal013@gmail.com>
2025-12-06 15:06:23 +0530

    Update BlynkHandler.cpp

--- BlynkHandler.cpp
+++ BlynkHandler.cpp
@@ -15,17 +15,14 @@ void BlynkHandler::run() {
     _timer->run();
 }
 
-void BlynkHandler::sendSensorData(float pm1_0, float pm2_5, float pm10_0) {
+void BlynkHandler::sendData(float pm1_0, float pm2_5, float pm10_0, float temperature, float humidity) {
+    Blynk.beginGroup();
+    
     Blynk.virtualWrite(V0, pm1_0);
     Blynk.virtualWrite(V1, pm2_5);
     Blynk.virtualWrite(V2, pm10_0);
-}
-
-void BlynkHandler::sendTemperatureHumidity(float temperature, float humidity) {
     Blynk.virtualWrite(V4, temperature);
     Blynk.virtualWrite(V5, humidity);
-}
 
-void BlynkHandler::sendFirmwareVersion(const char* version) {
-    Blynk.virtualWrite(V3, version);
+    Blynk.endGroup();
 }
