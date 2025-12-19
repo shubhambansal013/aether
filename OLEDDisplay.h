e7f250ac8338e48322bc65ea249c199214bf226b
shubhambansal013 <shubhambansal013@gmail.com>
2025-12-19 14:32:19 +0530

    Update OLEDDisplay.h

--- OLEDDisplay.h
+++ OLEDDisplay.h
@@ -17,19 +17,18 @@ public:
     void clear();
     void printMessage(String line1, String line2);
 
-    // The single point of entry for rendering the dashboard
+    // FIXED: Added int remainingSeconds (Total 9 arguments)
     void update(String wifiStatus, float pm1_0, float pm2_5, float pm10_0, 
-                float temp, float hum, bool isWarmup, bool isSleeping);
+                float temp, float hum, bool isWarmup, bool isSleeping, int remainingSeconds);
 
 private:
     Adafruit_SSD1306 display;
     
-    // Internal Rendering Sections
-    void drawStatusBar(String wifiStatus, bool isWarmup, bool isSleeping);
+    // FIXED: Added int countdown
+    void drawStatusBar(String wifiStatus, bool isWarmup, bool isSleeping, int countdown);
     void drawHeroSection(float pm2_5);
     void drawSecondaryGrid(float pm1_0, float pm10_0, float temp, float hum);
     
-    // Icon Drawing Helpers
     void drawWifiIcon(int16_t x, int16_t y, String status);
     void drawModeIcon(int16_t x, int16_t y, bool isWarmup);
     void drawFanIcon(int16_t x, int16_t y, bool isFanOn);
