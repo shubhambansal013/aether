3701a0d71cb9970997e530148a25711280ff3b57
shubhambansal013 <shubhambansal013@users.noreply.github.com>
2026-05-29 05:13:06 +0000

    deploy: 447737deb83db1082259e3f432aa647844458dd8

--- Config.h
+++ Config.h
@@ -33,10 +33,10 @@ static const unsigned long DEBOUNCE_DELAY          = 50;
 static const unsigned long BUTTON_LONG_PRESS_TIME  = 2000; 
 
 // --- OTA Settings ---
-static const unsigned long OTA_CHECK_INTERVAL = 60000; // 1 min for testing — change to 3600000 (1hr) before production
+static const unsigned long OTA_CHECK_INTERVAL = 600000; // 10 mins
 static const char* OTA_MANIFEST_URL = "https://shubhambansal013.github.io/aether/version.json";
 
 // --- Meta ---
-static const char* FIRMWARE_VERSION = "931e9ee"; // overwritten by CI via sed before compile — do not change manually
+static const char* FIRMWARE_VERSION = "447737d"; // overwritten by CI via sed before compile — do not change manually
 
 #endif
